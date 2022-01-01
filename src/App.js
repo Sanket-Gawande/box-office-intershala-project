8b1233e35b7b7335d3a602fe2dd13e28ef31eea6
Sanket-Gawande <sanketgawande.gcoey@gmail.com>
2022-01-01 13:17:08 +0530

    added some component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-
+import Home from './pages/Home'
+import Starred from './pages/Starred'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
 
 const App = () => {
     return (
-        <>
-            <Navbar/>
-            
-        </>
+       <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/starred' element={<Starred/>}/>
+            </Routes>
+       </BrowserRouter>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
-import Home from '../pages/Home'
-import Starred from '../pages/Starred'
 
 const Navbar = () => {
     return (
-        <BrowserRouter>
-                <ul>
-                    <li>
-                        <Link exact to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link exact to='/starred'>Starred</Link>
-                    </li>
-                </ul>
-            <Routes>
-                <Route exact path='/' element={<Home/>}/>
-                <Route exact path='/starred' element={<Starred/>}/>
-            </Routes>
-        </BrowserRouter>
+        <>  <h4>Are you looking for a movie or actor</h4>
+            <ul>
+                <li>
+                    <Link exact to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link exact to='/starred'>Starred</Link>
+                </li>
+            </ul>
+        </>
+
     )
 }
 
